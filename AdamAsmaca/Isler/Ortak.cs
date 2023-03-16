009b3f6c51570f788c5df27686d8a8b59e74fa33
EVREN YALÇIN <103932803+EVRENYALCIN@users.noreply.github.com>
2023-03-16 15:47:19 +0300

    Add files via upload

---
+++ AdamAsmaca/Isler/Ortak.cs
@@ -0,0 +1,34 @@
+﻿using AdamAsmaca.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamAsmaca.Isler
+{
+    public class Ortak
+    {
+		private static List<Kullanici> kullanicilar;
+
+		public static List<Kullanici> Kullanicilar
+        {
+			get {
+                if (kullanicilar == null)
+                {
+                    kullanicilar = new List<Kullanici>();
+                    //#if DEBUG
+                    //kullanicilar.Add(new Kullanici
+                    //{
+                    //    KullaniciAdi = "evren",
+                    //    Sifre = "1234"
+                    //});
+                    //#endif
+                }
+                return kullanicilar;
+            }
+		}
+
+        public static Kullanici AktifKullanici { get; set; }
+    }
+}
